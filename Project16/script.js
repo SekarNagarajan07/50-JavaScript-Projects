bdbbd388167ec1229356e9823968e616e87052fa
sekar N <sekarnagarajan1234@gmail.com>
2023-09-03 12:47:11 +0530

    js

--- Project16/script.js
+++ Project16/script.js
@@ -2,6 +2,13 @@ const smallCups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+updateBigCup();
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => {
+    highlightCups(idx);
+  });
+});
 
 function highlightCups(idx) {
   if (idx == 7 && smallCups[idx].classList.contains("full")) idx--;
@@ -18,6 +25,27 @@ function highlightCups(idx) {
       cup.classList.remove("full");
     }
   });
+  updateBigCup();
 }
 
-function updateBigCup() {}
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 300}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
+}
