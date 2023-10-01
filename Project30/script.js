c5de1ab957b31b702e072ec1080382f2f02a54a8
sekar N <sekarnagarajan1234@gmail.com>
2023-10-01 17:53:21 +0530

    js

---
+++ Project30/script.js
@@ -0,0 +1,20 @@
+const textE1 = document.getElementById("text");
+const speedE1 = document.getElementById("speed");
+const text = "We Love Coding";
+let idx = 1;
+let speed = 300 / speedE1.value;
+
+function writeText() {
+  textE1.innerText = text.slice(0, idx);
+  idx++;
+
+  if (idx > text.length) {
+    idx = 1;
+  }
+  setTimeout(writeText, speed);
+}
+writeText();
+
+speedE1.addEventListener("input", (e) => {
+  speed = 300 / e.target.value;
+});
